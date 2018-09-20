aa90b0caa23938c1a0ac3d98fa46c670c0c830d5
Brandon Laing <blaing2@stumail.jccc.edu>
2018-09-19 20:32:08 -0500

    Worked on main menu save system

---
+++ Assets/Scripts/SaveSystem/LocationSaveData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/** LocationSaveData:
+ * Created by: Brandon Laing
+ * Edited by: Brandon Laing
+ * 
+ * This is a data type that hold the name of a scene and a checkpoint location within that scene
+ */
+[CreateAssetMenu]  // this allows us to make this data type from the asset menu
+public class LocationSaveData : ScriptableObject
+{
+  #region Variables
+  public string sceneName;
+  public int checkpointNumber;
+
+  private string defaultSceneName = "Scene01";
+  private int defaultCheckpointNumber = 0;
+  #endregion
+
+  #region Constructors
+  /** BaseConstructor
+   * Makes new saveLocation with default sceneName and checkpointNumber
+   */
+  public LocationSaveData()
+  {
+    sceneName = defaultSceneName;
+    checkpointNumber = defaultCheckpointNumber;
+
+  }
+
+  /** @param: scene, @param checkpoint
+   * this takes in a scene and checkpoint and makes location save data from it.
+   * The checkpoint just moves into the checkpoint number and the sceneName
+   * is taken from the @scene
+   */
+  public LocationSaveData(Scene scene, int checkpoint)
+  {
+    sceneName = scene.name;
+    checkpointNumber = checkpoint;
+
+  }
+
+  /** @param: name, @param checkpoint
+   * Takes name and puts it into sceneName and moves checkpoint into checkpointNumber
+   */
+  public LocationSaveData(string name, int checkpoint)
+  {
+    sceneName = name;
+    checkpointNumber = checkpoint;
+
+  }
+  #endregion
+}
---
+++ Assets/Scripts/UI/SaveFileUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveFileUpdater : MonoBehaviour
+{
+  public Text fileNameUI;
+  public Text sceneNameUI;
+  public Text loadPointUI;
+
+  public string fileName;
+  public string sceneName;
+  public string loadPoint;
+
+  public void UpdateInfo()
+  {
+    fileNameUI.text = fileName;
+    sceneNameUI.text = sceneName;
+    loadPointUI.text = loadPoint;
+  }
+  public void SetInfo(string fileName, string sceneName, string loadPoint)
+  {
+    this.fileName = fileName;
+    this.sceneName = sceneName;
+    this.loadPoint = loadPoint;
+
+    UpdateInfo();
+  }
+
+  public void _LoadClickedScene()
+  {
+    Debug.Log("SHIT");
+
+    MainMenuManager.mainMenuManager._LoadScene(fileName);
+
+  }
+}
