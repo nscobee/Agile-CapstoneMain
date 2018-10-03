a350ace98f66724060da1c553cbb0b1736f1633d
neidt <35543916+neidt@users.noreply.github.com>
2018-10-03 12:40:26 -0500

    ranged ai test scripts/assets

---
+++ Assets/CheckForPlayerHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForPlayerHit : MonoBehaviour
+{
+    playerController playerScript;
+    EnemyAI enemyai;
+    //MageAI mageai;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Player hit, taking damage");
+        playerScript.takeDamage(enemyai.damageAmount);
+        this.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>();
+        enemyai = GameObject.FindGameObjectWithTag("enemy").GetComponent<EnemyAI>();
+        //mageai = GameObject.FindGameObjectWithTag("mage").GetComponent<MageAI>();
+    }
+	
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(enemyai.bullet.transform.position, enemyai.bulletSplashArea);
+    //}
+}
---
+++ Assets/scripts/MageAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageAI : EnemyAI{
+
+    public float timeTillAttack;
+    public float startTimeTillAttack;
+    public LayerMask playerLayer;
+    public Transform fireball;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        if(playerInRange)
+        {
+            ChasePlayer();
+            //attack with fireball
+            if (timeTillAttack < 0)
+            {
+                Attack();
+                Collider2D[] thingsToHit = Physics2D.OverlapCircleAll(fireball.transform.position, bulletSplashArea, playerLayer);
+                for (int i = 0; i < thingsToHit.Length; i++)
+                {
+                    thingsToHit[i].GetComponent<playerController>().takeDamage(damageAmount);
+                }
+            }
+        }
+    }
+
+    
+}
