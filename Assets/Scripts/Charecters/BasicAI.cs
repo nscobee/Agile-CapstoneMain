caffd70ca67d3248b1c97850d46e21231128d954
Natalie Eidt <neidt@yahoo.com>
2018-11-01 08:17:43 -0500

    first wander test

--- Assets/Scripts/Charecters/BasicAI.cs
+++ Assets/Scripts/Charecters/BasicAI.cs
@@ -4,111 +4,110 @@ using UnityEngine;
 
 public class BasicAI : MonoBehaviour
 {
-  public List<Transform> patrolPoints = new List<Transform>();
+    public List<Transform> patrolPoints = new List<Transform>();
 
-  private Transform currentTarget;
+    private Transform currentTarget;
 
-  public static int speed = 5;
-  public static int stopDistance = 1;
+    public static int speed = 5;
+    public static int stopDistance = 1;
 
-  public bool gatherPoints = false;
+    public bool gatherPoints = false;
 
-  public BasicMovement playerMovement;
+    public BasicMovement playerMovement;
 
-  public AISpawner homeSpawner;
+    public AISpawner homeSpawner;
 
     public GameObject phantom;  //Obtain info about phantom to have it persist
     public BoxCollider phantomBox; //to be hidden while phantom is possessing
     public MeshRenderer phantomMesh; //same as ^^
-    
 
-  private void Start()
-  {
-    // if you want points to be gathered it does that
-    if (gatherPoints)
+
+    private void Start()
     {
-      GenericFunctions.GatherComponetFromSceneByTag<Transform>(ref patrolPoints, "PatrolPoint");
-
-
-    }
+        // if you want points to be gathered it does that
+        if (gatherPoints)
+        {
+            GenericFunctions.GatherComponetFromSceneByTag<Transform>(ref patrolPoints, "PatrolPoint");
+        }
 
         phantom = GameObject.FindWithTag("Player");
         phantomBox = phantom.GetComponent<BoxCollider>();
         phantomMesh = phantom.GetComponent<MeshRenderer>();
-  }
-
-  private void Update()
-  {
-    // if there is no target it gets one
-    if (currentTarget == null)
-    {
-      if (patrolPoints.Count > 0)
-      {
-        currentTarget = FindTarget(patrolPoints);
-
-      }
-
     }
 
-    if (currentTarget != null)
+    private void Update()
     {
-      // Move toward the target
-      this.transform.position = MoveTowardsObject(this.transform.position, currentTarget.transform.position, speed);
+        // if there is no target it gets one
+        if (currentTarget == null)
+        {
+            if (patrolPoints.Count > 0)
+            {
+                currentTarget = FindTarget(patrolPoints);
+            }
+        }
 
-      // checks if its too close to target
-      if (CheckToStop(this.transform, currentTarget.transform, stopDistance))
-      {
-        currentTarget = null;
+        if (currentTarget != null)
+        {
+            // Move toward the target
+            this.transform.position = MoveTowardsObject(this.transform.position, currentTarget.transform.position, speed);
 
-      }
+            // checks if its too close to target
+            if (CheckToStop(this.transform, currentTarget.transform, stopDistance))
+            {
+                currentTarget = null;
+            }
+        }
     }
-  }
 
-  // when the player possess a AI it destories the phantom and enables the player movement on the 
-  public void Possess(GameObject phantom)
-  {
+    // when the player possess a AI it destories the phantom and enables the player movement on the 
+    public void Possess(GameObject phantom)
+    {
         phantomBox.enabled = false; //hide the phantom without nuking him
         phantomMesh.enabled = false; //^^^same
 
         phantom.transform.position = this.transform.position; //reset phantom's position to currently possessed NPC
-    
 
-    playerMovement.enabled = true;
-    this.enabled = false;
-
-  }
-
-  // this will be called from update if the AI has no target and will get a target from the given List
-  public Transform FindTarget(List<Transform> transformList)
-  {
-    int randomNumber = Random.Range(0, patrolPoints.Count);
-
-    return transformList[randomNumber];
-
-  }
-
-  // Checks if two transform points are within a certain distance of each other
-  public bool CheckToStop(Transform currentPosition, Transform targetPosition, float distance)
-  {
-    return Vector3.Distance(currentPosition.position, targetPosition.position) < distance;
-
-  }
-
-  // moves one object towards another by set speed
-  public Vector3 MoveTowardsObject(Vector3 mover, Vector3 target, float speed)
-  {
-    return Vector3.MoveTowards(mover, target, speed * Time.deltaTime);
-
-  }
-
-  // this is to let the spawner know that it can send out another AI
-  private void OnDestroy()
-  {
-    if (homeSpawner)
-    {
-      homeSpawner.AI.Remove(this.gameObject);
-
+        playerMovement.enabled = true;
+        this.enabled = false;
     }
-  }
 
+    // this will be called from update if the AI has no target and will get a target from the given List
+    public Transform FindTarget(List<Transform> transformList)
+    {
+        int randomNumber = Random.Range(0, patrolPoints.Count);
+
+        return transformList[randomNumber];
+    }
+
+    // Checks if two transform points are within a certain distance of each other
+    public bool CheckToStop(Transform currentPosition, Transform targetPosition, float distanceOfSat)
+    {
+        return Vector3.Distance(currentPosition.position, targetPosition.position) < distanceOfSat;
+    }
+
+    // moves one object towards another by set speed
+    public Vector3 MoveTowardsObject(Vector3 mover, Vector3 target, float speed)
+    {
+        float targetXVariance = Random.Range(-5 + target.x, 5 + target.x);
+        float targetYVariance = Random.Range(-5 + target.y, 5 + target.y);
+        Vector3 targetWanderArea = new Vector3(targetXVariance, targetYVariance);
+
+        if(Vector3.Distance(mover, target) < 2)
+        {
+            targetXVariance = Random.Range(-target.x, target.x);
+            targetXVariance = Random.Range(-target.y, target.y);
+        }
+        
+        return Vector3.MoveTowards(mover, targetWanderArea, speed * Time.deltaTime);
+        
+    }
+
+    // this is to let the spawner know that it can send out another AI
+    private void OnDestroy()
+    {
+        if (homeSpawner)
+        {
+            homeSpawner.AI.Remove(this.gameObject);
+        }
+    }
 }
