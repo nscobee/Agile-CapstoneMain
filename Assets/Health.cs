369bb80a1d25f24eb5ec54a5d081934eb3234255
ceflin <ceflin@stumail.jccc.edu>
2018-11-07 13:00:25 -0600

    Sprint 3, still in test phase

---
+++ Assets/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public const float MAX_HEALTH = 100.0f;
+    public float currentHealth;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = MAX_HEALTH;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        TakeDamage(10.0f);
+    }
+}
---
+++ Assets/Scripts/Charecters/AIHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHealth : MonoBehaviour
+{
+    public const float MAX_HEALTH = 100;
+    public float currentHealth;
+
+    public dealDamage damageScript;
+
+    private float incomingDamage;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float amount)
+    {
+        incomingDamage = amount;
+        currentHealth -= incomingDamage;
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
