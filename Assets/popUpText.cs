976be648949a752cc46cc95610fe523d5e382138
svc-student <svc-student@student.directory.jccc>
2019-03-04 11:11:54 -0600

    Pop Up Text Implemented

---
+++ Assets/popUpText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popUpText : MonoBehaviour
+{
+    public Text popUpMessageText;
+    public GameObject popUpMessagePanel;
+    public string message;
+    [Tooltip("The exact name of the prefab you want to trigger the speech bubble")]
+    public string triggerPrefabString;
+
+    public bool destroyOnLeave = false;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpMessagePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.gameObject == GameObject.Find(triggerPrefabString))
+        {
+            popUpMessagePanel.SetActive(true);
+            popUpMessageText.text = message;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(!destroyOnLeave)
+        popUpMessagePanel.SetActive(false);
+        else
+        Destroy(popUpMessagePanel);
+    }
+}
