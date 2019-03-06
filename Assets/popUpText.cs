01c40482d5eb2ff4b58f523fe4a2d47ab6077ccc
svc-student <svc-student@student.directory.jccc>
2019-03-06 10:42:52 -0600

    popup script edited slightly

--- Assets/popUpText.cs
+++ Assets/popUpText.cs
@@ -7,33 +7,35 @@ public class popUpText : MonoBehaviour
 {
     public Text popUpMessageText;
     public GameObject popUpMessagePanel;
-    public string message;
+    public string regularMessage;
+    public string phantomSpecificMessage;
     [Tooltip("The exact name of the prefab you want to trigger the speech bubble")]
     public string triggerPrefabString;
 
     public bool destroyOnLeave = false;
     private bool hasTriggered = false;
     public bool triggerOnAnyNPC = false;
-   
+
+    PhantomControls phantom;
 
     // Start is called before the first frame update
     void Start()
     {
+        phantom = GameObject.Find("Phantom2.0").GetComponent<PhantomControls>();
         popUpMessagePanel.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
        if(other.gameObject == GameObject.Find(triggerPrefabString) || triggerOnAnyNPC)
         {
             popUpMessagePanel.SetActive(true);
-            popUpMessageText.text = message;
+            //if the phantom isnt possessing, display phantom message
+            //else display character's reaction to another
+            if (!phantom.isPossessing)
+                popUpMessageText.text = phantomSpecificMessage;
+            else
+                popUpMessageText.text = regularMessage;
             hasTriggered = true;
         }
     }
