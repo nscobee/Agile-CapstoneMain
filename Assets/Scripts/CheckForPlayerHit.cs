5cfb268d18d4be21805c2330ab647b8a784bde07
svc-student <svc-student@student.directory.jccc>
2019-03-11 10:29:27 -0500

    added lock and key scripterino

---
+++ Assets/Scripts/CheckForPlayerHit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForPlayerHit : MonoBehaviour
+{
+    playerHealth playerScript;
+    MageAI mageai;
+    AIHealth aiHealth;
+    BasicAI basicAI;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit a thing");
+        if (other.tag == "Player")
+        {
+            playerScript = other.GetComponent<playerHealth>();
+            Debug.Log("Player hit, taking damage");
+            //playerScript.takeDamage(mageai.fireballDamageAmount);
+            this.enabled = false;
+        }
+
+        if (other.tag == "mage")
+        {
+            aiHealth = other.GetComponent<AIHealth>();
+            mageai = other.GetComponent<MageAI>();
+
+            aiHealth.TakeDamage(mageai.fireballDamageAmount);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<playerHealth>();
+        //basicAI = GameObject.FindGameObjectWithTag("mage").GetComponent<BasicAI>();
+        //get if mage info
+        //mageai = GameObject.FindGameObjectWithTag("mage").GetComponent<MageAI>();
+    }
+
+}
---
+++ Assets/Scripts/keyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyScript : MonoBehaviour
+{
+    public GameObject matchingDoor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject == GameObject.Find("Phantom2.0") || other.gameObject.tag == "Player")
+        {
+            Destroy(matchingDoor);
+            Destroy(this.gameObject);
+
+        }
+    }
+}
