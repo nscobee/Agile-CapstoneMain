e4d9bc9f3aae7d523df1905cdd2c46e91195d9ad
svc-student <svc-student@student.directory.jccc>
2019-03-11 11:01:19 -0500

    spirit guide done

--- Assets/SpiritGuide.cs
+++ Assets/SpiritGuide.cs
@@ -6,35 +6,25 @@ public class SpiritGuide : MonoBehaviour
 {
     public List<Transform> pointsList = new List<Transform>();
     public float dialogueWait = 7f;
-    public float speed = 5f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(FollowPoints(this.gameObject.transform, pointsList));
-    }
+    public float speed = 1.5f;
+    private int count = 0;
 
     // Update is called once per frame
     void Update()
     {
-       
-    }
-    IEnumerator FollowPoints(Transform tutorialGhost, List<Transform> targets)
-    {
-        yield return new WaitForSeconds(dialogueWait);
-        for (int i = 0; i < pointsList.Count; i++)
+        if (Vector3.Distance(this.transform.position, pointsList[count].transform.position) > .3f)
         {
-            if (Vector2.Distance(tutorialGhost.position, pointsList[i].position) > 1f)
-            {
-                tutorialGhost.transform.position = Vector3.MoveTowards(tutorialGhost.position, pointsList[i].position, speed);
-            }
-            yield return new WaitForSecondsRealtime(3f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pointsList[count].transform.position, speed * Time.deltaTime);
+
         }
-            
+        else
+        {
+            count += 1;
+        }
 
-        yield return new WaitForSeconds(10f);
-        Destroy(this.gameObject);
+        if(count >= 5)
+        {
+            Destroy(this.gameObject);
+        }
     }
-
 }
