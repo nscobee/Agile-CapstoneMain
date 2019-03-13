ea575148f6f63b2ad75b79d02d90c71fbdee63dd
Natalie Eidt <neidt@yahoo.com>
2019-03-12 19:39:10 -0500

    ui working for melee in nicks 003 test scene

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public PhantomControls phantomControls;
+    public GameObject uiObj;
+    Canvas theUI;
+
+    private void Start()
+    {
+        theUI = uiObj.GetComponent<Canvas>();
+        theUI.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (phantomControls.isPossessing && this.gameObject.GetComponent<BasicAI>().playerInRangeR)
+        {
+            theUI.enabled = true;
+        }
+        else
+        {
+            uiObj.SetActive(true);
+            theUI.enabled = false;
+        }
+    }
+}
