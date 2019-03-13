7031716bcc087cf5600e258d717970d29eb38571
Natalie Eidt <neidt@yahoo.com>
2019-03-13 10:43:03 -0500

    ai fixed

--- Assets/Scripts/SpiritGuide.cs
+++ Assets/Scripts/SpiritGuide.cs
@@ -8,23 +8,38 @@ public class SpiritGuide : MonoBehaviour
     public float dialogueWait = 7f;
     public float speed = 1.5f;
     private int count = 0;
-
+    private bool playerIsInRange = false;
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, pointsList[count].transform.position) > .3f)
+        if (Time.time >= dialogueWait && playerIsInRange)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, pointsList[count].transform.position, speed * Time.deltaTime);
+            if (Vector3.Distance(this.transform.position, pointsList[count].transform.position) > .3f)
+            {
+                this.transform.position = Vector3.MoveTowards(this.transform.position, pointsList[count].transform.position, speed * Time.deltaTime);
 
-        }
-        else
-        {
-            count += 1;
+            }
+            else
+            {
+                count += 1;
+            }
         }
 
         if(count >= 5)
         {
             Destroy(this.gameObject);
         }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Phantom2.0")
+        {
+            playerIsInRange = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerIsInRange = false;
     }
 }
