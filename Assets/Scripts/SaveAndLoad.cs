c56a445e9636d2afa8f824e8ea44e2309835356f
svc-student <svc-student@student.directory.jccc>
2019-04-01 11:30:27 -0500

    worked on save sys (again)

--- Assets/Scripts/SaveAndLoad.cs
+++ Assets/Scripts/SaveAndLoad.cs
@@ -7,22 +7,23 @@ using System.IO;
 public static class SaveAndLoad
 {
     public static List<LevelData> savedGames = new List<LevelData>();
-    
+    public static string saveNameFolder = "ThisIsATest";
+
     public static void Save()
     {
         savedGames.Add(LevelData.current);
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.txt");
+        FileStream file = File.Create(Application.persistentDataPath + "/" + saveNameFolder + "/savedGames.txt");
         bf.Serialize(file, SaveAndLoad.savedGames);
         file.Close();
     }
 
-    public static void Load()
+    public static void Load(string levelNameFolder)
     {
-        if(File.Exists(Application.persistentDataPath + "/savedGames.txt"))
+        if(File.Exists(Application.persistentDataPath +"/" + levelNameFolder + "/savedGames.txt"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.txt", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/" + levelNameFolder + "/savedGames.txt", FileMode.Open);
             SaveAndLoad.savedGames = (List<LevelData>)bf.Deserialize(file);
             file.Close();
         }
--- Assets/Scripts/SaveLoadController.cs
+++ Assets/Scripts/SaveLoadController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveLoadController : MonoBehaviour
 {
     public static SaveLoadController control;
-
+    
     private LevelData levelData = new LevelData();
 
     public PhantomControls player;
@@ -26,15 +27,22 @@ public class SaveLoadController : MonoBehaviour
     
     public void SaveLevel(string levelName)
     {
+        Debug.Log("Save and load Controller saving");
+
         LevelData ld = new LevelData();
 
         PlayerData playerDat = new PlayerData();
-        
+
+        //scene stuff
+        ld.sceneNum = SceneManager.GetActiveScene().buildIndex;
+
+        //player stuff
         playerDat.currentHealth = playerHealthScript.currentHealth;
         playerDat.position = player.gameObject.transform;
         playerDat.isPossessing = player.isPossessing;
         ld.player = playerDat;
         
+        //melee stuff
         foreach (GameObject meleeEnemy in GameObject.FindGameObjectsWithTag("Melee"))
         {
             EnemyData enemyDat = new EnemyData();
@@ -43,6 +51,7 @@ public class SaveLoadController : MonoBehaviour
             ld.enemyList.Add(enemyDat);
         }
 
+        //mageStuff
         foreach (GameObject mageEnemy in GameObject.FindGameObjectsWithTag("mage"))
         {
             EnemyData enemyDat = new EnemyData();
@@ -50,7 +59,14 @@ public class SaveLoadController : MonoBehaviour
             enemyDat.currentHealth = mageEnemy.gameObject.GetComponent<AIHealth>().currentHealth;
             ld.enemyList.Add(enemyDat);
         }
-
+   
+        SaveAndLoad.Save();
     }
-    
+
+    public void LoadLevel(string levelName)
+    {
+        SaveAndLoad.Load(levelName);
+        SceneManager.LoadScene(levelData.sceneNum);
+    }
+
 }
