ab56d50dff93a704cd5d2c4ec1c94ae706f9f98f
svc-student <svc-student@student.directory.jccc>
2019-05-15 11:25:15 -0500

    added level up animation

---
+++ Assets/animateLevelUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateLevelUp : MonoBehaviour
+{
+    public GameObject phantom;
+    public Animator anim;
+    public levelingScript levelScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        phantom = GameObject.FindGameObjectWithTag("Player");
+        levelScript = phantom.GetComponent<levelingScript>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelScript.currentLevel >= levelScript.xpTillNextLevel)
+            anim.SetTrigger("levelUp");
+    }
+}
